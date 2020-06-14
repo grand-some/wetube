0217f7e2a388b21df8324f4560b534f851e8b7dd
grand-some <koc7177@gmail.com>
2020-06-14 17:06:38 +0900

    app.get function

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const PORT=4000;
 function handleListening(){
     console.log(`listening on : http://localhost:${PORT}`)
 }
-app.listen(PORT, handleListening)
+
+function handleHome(req, res){
+    console.log("Hi from Home")
+    res.send("hi kwon")
+}
+app.get("/", handleHome)
+app.listen(PORT, handleListening);
