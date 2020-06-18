ef76c29364387747882d499d7f39a1d85b0de5f2
kwon <koc7177@gmail.com>
2020-06-18 22:11:11 +0900

    lecture 2-12

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-const express=require('express');
-const morgan=require('morgan');
-const helmet=require('helmet');
-const cookieParser =require('cookie-parser');
-const bodyParser = require('body-parser');
+import express from'express'
+import morgan from'morgan'
+import helmet from'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import userRouter from './router/userRouter'
+import videoRouter from './router/videoRouter'
+import globalRouter from './router/globalRouter'
+import routes from './routes'
+
 const app = express();
-
-import {userRouter} from "./router"
-
-const handleHome= (req, res)=>{res.send("hi kwon you are genius")}
-const handleProfile = (req, res)=>{res.send("you are on my profile")};
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet()); 
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use(routes.home, globalRouter);
+app.use(routes.user, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.use("/user", userRouter);
 export default app;
---
+++ router/videoRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from '../routes';
+import { videos, upload, videoDetail, editVideo, deleteVideos } from '../controllers/videoControllers';
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideos);
+export default videoRouter;
